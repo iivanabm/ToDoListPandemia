5cceb352f7dfb2c3c2d44b5e63f4af019553d4fa
iivanabm <ivanammarinho@hotmail.com>
2021-10-03 12:21:38 -0300

    mudança na tarefa renderizada

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,8 +4,6 @@ const Todo = ({ text }) => {
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
-      <button className="complete-btn"><i className="fas fa-check"></i></button>
-      <button className="trash-btn"><i className="fas fa-trash"></i></button>
     </div>
   );
 };
